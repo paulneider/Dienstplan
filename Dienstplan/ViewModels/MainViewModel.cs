6931b43773c95a4e5994bdee5fce2ab80ccd7cf8
paulneider <paul.neider@mail.de>
2023-04-22 17:46:41 +0200

    Fix Group Name

--- Dienstplan/ViewModels/MainViewModel.cs
+++ Dienstplan/ViewModels/MainViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 using Microsoft.EntityFrameworkCore;
