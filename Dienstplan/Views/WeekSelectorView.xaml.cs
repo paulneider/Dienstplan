1f973a258ac82b5f3a40045b99329f82b32cb324
paulneider <paul.neider@mail.de>
2023-05-05 17:03:10 +0200

    Start WeekSelectorView

---
+++ Dienstplan/Views/WeekSelectorView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System;
+using System.Windows.Controls;
+
+namespace Dienstplan;
+public partial class WeekSelectorView : UserControl
+{
+    public WeekSelectorView()
+    {
+        InitializeComponent();
+        calendar.SelectedDatesChanged += Calendar_SelectedDatesChanged;
+    }
+    private void Calendar_SelectedDatesChanged(object? sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        calendar.SelectedDatesChanged -= Calendar_SelectedDatesChanged;
+
+        DateTime date = e.AddedItems.OfType<DateTime>().FirstOrDefault();
+        if (date == default)
+            return;
+
+        int daysAfterMonday = ((int)date.DayOfWeek) - 1;
+
+        // if sunday -> daysAfterMonday = -1 -> other week then last monday
+        daysAfterMonday = daysAfterMonday == -1 ? 6 : daysAfterMonday;
+
+        // subtract daysAfterMonday to get the monday of the selected week
+        DateTime start = date.AddDays(-daysAfterMonday);
+        
+        calendar.SelectedDates.Clear();
+        calendar.SelectedDates.AddRange(start, start.AddDays(6));
+        
+        calendar.SelectedDatesChanged += Calendar_SelectedDatesChanged;
+    }
+}
+
